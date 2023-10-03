a23919e071a9b9ee2d3408397a81dec850e5d0e0
Alex Petrenko <alex.pt@ya.ru>
2023-10-03 19:42:29 +0300

    Добавить фоновое изображение

--- SchoolhouseSkateboarder/GameScene.swift
+++ SchoolhouseSkateboarder/GameScene.swift
@@ -5,6 +5,14 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        anchorPoint = CGPoint.zero
+        
+        // добавляем фоновое изображение
+        let background = SKSpriteNode(imageNamed: "background")
+        let xMid = frame.midX
+        let yMid = frame.midY
+        background.position = CGPoint(x: xMid, y: yMid)
+        addChild(background)
         }
         
     override func update(_ currentTime: TimeInterval) {
