c16909ea05a433b169490dbac84786f99248dae1
Alex Petrenko <alex.pt@ya.ru>
2023-10-04 14:40:13 +0300

    Создать героя игры и поместить его в сцену

--- SchoolhouseSkateboarder/GameScene.swift
+++ SchoolhouseSkateboarder/GameScene.swift
@@ -1,7 +1,9 @@
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
+    
+    // создаем героя игры - скейтбордистку
+    let skater = Skater(imageNamed: "skater")
     
     override func didMove(to view: SKView) {
         
@@ -13,9 +15,24 @@ class GameScene: SKScene {
         let yMid = frame.midY
         background.position = CGPoint(x: xMid, y: yMid)
         addChild(background)
+        
+        // настраиваем свойства скейтбордистки и добавляем ее в сцену 
+        resetSkater()
+        addChild(skater)
         }
         
+    func resetSkater() {
+        // задаем начальное положение скейтбордистки, zPosition и minimumY
+        let skaterX = frame.midX / 2.0
+        let skaterY = skater.frame.height / 2.0 + 64.0
+        skater.position = CGPoint(x: skaterX, y: skaterY)
+        skater.zPosition = 10
+        skater.minimumY = skaterY
+        
+    }
+    
     override func update(_ currentTime: TimeInterval) {
+        // вызывается перед отрисовкой каждого кадра
 
     }
 }
